996ec8a6d3a556114d48e20ea53f5eb790799f49
Funsho <oluwafunshoalabi97@gmail.com>
2025-07-22 10:48:29 +0100

    fully merged

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // desktop functions 
 
 
@@ -12,19 +11,4 @@ menuBtn.addEventListener('click', function(){
 })
 closeBtn.addEventListener('click', function(){
     mobileNav.style.display = 'none';
-=======
-// desktop functions 
-
-
-// mobile functions 
-let menuBtn = document.getElementById('menu');
-let mobileNav = document.getElementById('mobile-nav');
-let closeBtn = document.getElementById('close')
-
-menuBtn.addEventListener('click', function(){
-    mobileNav.style.display = 'block';
-})
-closeBtn.addEventListener('click', function(){
-    mobileNav.style.display = 'none';
->>>>>>> 72351f2fa06ed21c9ccccc94c3a90ea5dd560ccd
 })
