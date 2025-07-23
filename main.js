966e299fc5a2bef00d7e758a1ab14da6edf2e84e
Funsho <oluwafunshoalabi97@gmail.com>
2025-07-23 11:47:44 +0100

    home page implementation complete

--- main.js
+++ main.js
@@ -21,16 +21,16 @@ closeBtn.addEventListener('click', function(){
 const images = [
     { image: 'https://iili.io/FOlBhtR.jpg',
         name: 'John Wick',
-        description: 'My experience was nothing short but amazing. The staff were warm, welcoming and alwasy ready to help. The romm was clean, stylish and incredibly comfortable. I expecially enjoyed the breakfast.' 
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quas explicabo maiores, fugit necessitatibus sit corrupti distinctio est perferendis corporis odio. Odit omnis voluptate nesciunt deleniti eveniet placeat nobis sit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.' 
 
     },
     { image: 'https://iili.io/FOlBhtR.jpg',
-        name: 'John Wick',
+        name: 'Susan Claw',
         description: 'My experience was nothing short but amazing. The staff were warm, welcoming and alwasy ready to help. The romm was clean, stylish and incredibly comfortable. I expecially enjoyed the breakfast.' 
 
     },
     { image: 'https://iili.io/FOlBhtR.jpg', 
-        name: 'John Wick',
+        name: 'Topaz Funsho ',
         description: 'My experience was nothing short but amazing. The staff were warm, welcoming and alwasy ready to help. The romm was clean, stylish and incredibly comfortable. I expecially enjoyed the breakfast.' 
 
     }
@@ -42,16 +42,25 @@ const images = [
     const div = document.createElement('div');
     div.className = 'carousel-item' + (index === 0 ? ' active' : '');
     const img = document.createElement('img');
+    const p = document.createElement('p');
+    const h3 = document.createElement('h3');
+    p.innerText = `${item.description}`
+    p.style.maxWidth = '800px'
+    p.style.justifySelf = 'center'
+    p.style.textAlign = 'center'
     img.src = `${item.image}`
-    img.className = 'd-block'
+    img.style.justifySelf = 'center'
+    img.classList.add('d-block')
+    img.classList.add('mt-5')
+    img.style.borderRadius = '100%'
+    img.style.width = '80px'
+    h3.innerText = `${item.name}`
+    h3.style.justifySelf = 'center'
+    h3.classList.add('mt-4')
+    div.appendChild(p)
     div.appendChild(img)
+    div.appendChild(h3)
 
-    // div.innerHTML = `
-    // <div class="carousel-caption d-none d-md-block">
-    //     <h5>${item.description}</h5>
-    //     <h4>${item.name}</h4>
-    //     </div>
-    // `;
 
     carouselInner.appendChild(div);
   });
