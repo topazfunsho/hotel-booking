77e6ef7f8aeaa0baa32b240106949b52e3072c8e
Funsho <oluwafunshoalabi97@gmail.com>
2025-07-23 14:06:25 +0100

    form validation

--- main.js
+++ main.js
@@ -6,39 +6,40 @@ let menuBtn = document.getElementById('menu');
 let mobileNav = document.getElementById('mobile-nav');
 let closeBtn = document.getElementById('close')
 
-menuBtn.addEventListener('click', function(){
+menuBtn.addEventListener('click', function () {
     mobileNav.style.display = 'block';
 })
-closeBtn.addEventListener('click', function(){
+closeBtn.addEventListener('click', function () {
     mobileNav.style.display = 'none';
 })
 
 
 
-
-
-// courosel section 
+// testimonial courosel section 
 const images = [
-    { image: 'https://iili.io/FOlBhtR.jpg',
+    {
+        image: 'https://iili.io/FOlBhtR.jpg',
         name: 'John Wick',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quas explicabo maiores, fugit necessitatibus sit corrupti distinctio est perferendis corporis odio. Odit omnis voluptate nesciunt deleniti eveniet placeat nobis sit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.' 
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora quas explicabo maiores, fugit necessitatibus sit corrupti distinctio est perferendis corporis odio. Odit omnis voluptate nesciunt deleniti eveniet placeat nobis sit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.'
 
     },
-    { image: 'https://iili.io/FOlBhtR.jpg',
+    {
+        image: 'https://iili.io/FOlBhtR.jpg',
         name: 'Susan Claw',
-        description: 'My experience was nothing short but amazing. The staff were warm, welcoming and alwasy ready to help. The romm was clean, stylish and incredibly comfortable. I expecially enjoyed the breakfast.' 
+        description: 'My experience was nothing short but amazing. The staff were warm, welcoming and alwasy ready to help. The romm was clean, stylish and incredibly comfortable. I expecially enjoyed the breakfast.'
 
     },
-    { image: 'https://iili.io/FOlBhtR.jpg', 
+    {
+        image: 'https://iili.io/FOlBhtR.jpg',
         name: 'Topaz Funsho ',
-        description: 'My experience was nothing short but amazing. The staff were warm, welcoming and alwasy ready to help. The romm was clean, stylish and incredibly comfortable. I expecially enjoyed the breakfast.' 
+        description: 'My experience was nothing short but amazing. The staff were warm, welcoming and alwasy ready to help. The romm was clean, stylish and incredibly comfortable. I expecially enjoyed the breakfast.'
 
     }
-  ];
+];
 
-  const carouselInner = document.getElementById('carouselInner');
+const carouselInner = document.getElementById('carouselInner');
 
-  images.forEach((item, index) => {
+images.forEach((item, index) => {
     const div = document.createElement('div');
     div.className = 'carousel-item' + (index === 0 ? ' active' : '');
     const img = document.createElement('img');
@@ -63,4 +64,33 @@ const images = [
 
 
     carouselInner.appendChild(div);
-  });
+});
+
+
+
+// home page booking form 
+
+
+const homeForm = document.getElementById('home-form');
+homeForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    let hotelLocation = document.getElementById('home-location').value;
+    let roomType = document.getElementById('home-type').value;
+    let checkIn = document.getElementById('date1').value;
+    // let checkOut = document.getElementById('date2').value;
+    alert('The Room type ' + roomType + ' at ' + hotelLocation + " with the check in date " + checkIn + ' is currently available/unavailable')
+})
+
+
+// news letter 
+const newsLetter = document.getElementById('news-letter-form');
+
+newsLetter.addEventListener('submit', function(e){
+    e.preventDefault();
+    let newsEmail = document.getElementById('news-letter-email').value;
+    if(newsEmail.includes('@')){
+        alert('Thank you for subscribing to out news letter')
+    }else{
+        alert('Please enter a valid email')
+    }
+})
